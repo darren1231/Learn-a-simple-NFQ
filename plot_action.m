3391259fed6cb0e66cc73615863135c380b9ef3e
Kun Da Lin <darrendarren1231@yahoo.com.tw>
2016-03-02 11:46:39 +0800

    visualize action value

---
+++ plot_action.m
@@ -0,0 +1,37 @@
+function [ q_value ] = plot_action( net )
+
+% number_experience=rand_experience;
+% [net_input,net_output] = transport_experience(number_experience);
+% net = train_rprop(net_input,net_output);
+
+for x=2:5
+      for y=2:5
+          for action=1:4
+            q_value(y,x,action)=roundn(sim(net,transport_feature_byte_diff(x,y,action)),-3);
+          end
+      end
+end
+plot(1,1);
+
+
+
+for center_x=0.2:0.5:1.7
+    for center_y=1.75:-0.5:0.25
+        x=(center_x-0.2)/0.5+2;
+        y=(1.75-center_y)/0.5+2;
+%     text(center_x,center_y+0.15,num2str(q_value((center_x-0.2)/0.5+2,(1.75-center_y)/0.5+2),1));
+%     text(center_x,center_y-0.15,num2str(q_value((center_x-0.2)/0.5+2,(1.75-center_y)/0.5+2),2));
+%     text(center_x-0.1,center_y,num2str(q_value((center_x-0.2)/0.5+2,(1.75-center_y)/0.5+2),3));
+%     text(center_x+0.1,center_y,num2str(q_value((center_x-0.2)/0.5+2,(1.75-center_y)/0.5+2),4));
+    text(center_x,center_y+0.15,num2str(q_value(y,x,1)));
+    text(center_x,center_y-0.15,num2str(q_value(y,x,2)));
+    text(center_x-0.1,center_y,num2str(q_value(y,x,3)));
+    text(center_x+0.1,center_y,num2str(q_value(y,x,4)));
+    
+%      text(center_x,center_y+0.15,num2str((1.75-center_y)/0.5+2));
+    end
+end
+
+
+end
+
